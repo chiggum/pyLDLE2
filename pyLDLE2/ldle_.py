5efda60d3d267c8056c44a6284560578e31631dc
chiggum <dhruvkohli99@gmail.com>
2022-07-21 15:55:32 -0700

    added doc, updated default params

--- pyLDLE2/ldle_.py
+++ pyLDLE2/ldle_.py
@@ -14,7 +14,31 @@ from . import visualize_
 from .util_ import print_log, sparse_matrix, nearest_neighbors
 import multiprocessing
     
-def double_manifold(X, ddX, k_nn):
+def double_manifold_k_nn(X, ddX, k_nn):
+    """Doubles the manifold represented by X and computes
+    k-nearest neighbors of each point on the double.
+    
+    Parameters
+    ----------
+    X : array shape (n_samples, n_features)
+        2d np.array of shape (n,p)
+        where each row represents a point.
+    ddX : array shape (n_samples,)
+          A 1d np.array of shape (n,) where ddX[i] = 0 iff the
+          i-th point X[i,:] represents a point on the boundary
+          of the manifold. Note that n_boundary = np.sum(ddX==0).
+    k_nn : int
+           Number of nearest neighbors to compute on the double.
+    
+    Returns
+    -------
+    neigh_dist : array shape (2*n_samples-n_boundary, k_nn)
+                 distance of k-nearest neighbors from each
+                 point on the double.
+    neigh_ind : array shape (2*n_samples-n_boundary, k_nn)
+                indices of k-nearest neighbors from each point
+                on the double.
+    """
     k_nn_ = 2*k_nn
     neigh = KNeighborsTransformer(n_neighbors=k_nn_, n_jobs=-1) #start with 2 x k_nn
     X_dist_graph = neigh.fit_transform(X)
@@ -123,61 +147,205 @@ def double_manifold(X, ddX, k_nn):
     return neigh_dist, neigh_ind
     
 
-default_local_opts = {'k_nn': 48, 'k_tune': 6, 'k': 24,
-                       'gl_type': 'unnorm', 'N': 100, 'no_gamma': False,
-                       'Atilde_method': 'LDLE_1', 'p': 0.99, 'tau': 50,
-                       'delta': 0.9, 'to_postprocess': True, 'algo': 'LDLE',
-                       'n_proc': max(1,int(multiprocessing.cpu_count()*0.75)),
-                       'pp_n_thresh': 32}
+def get_default_local_opts(algo='LDLE', k_nn=48, k_tune=6, k=24, gl_type='unnorm',
+                           N=100, no_gamma=False, Atilde_method='LDLE_1',
+                           p=0.99, tau=50, delta=0.9, to_postprocess= True,
+                           pp_n_thresh=32):
+    """Sets and returns a dictionary of default_local_opts.
+    
+    Parameters
+    ----------
+    algo : str
+           The algorithm to use for the construction of
+           local views. Options are 'LDLE' and 'LTSA'.
+           LTSA uses the hyperparameter k only and is
+           not affected by the value of the others.
+    k_nn : int
+           For k-nearest neighbor graph construction.
+    k_tune : int
+           Distance to k_tune-th nearest neighbor is
+           used as the local scaling factor. This must
+           be less than k_nn.
+    k : int 
+        The size of local view per point.
+    gl_type : str
+              The type of graph Laplacian to construct.
+              Options are 'unnorm' for unnormalized,
+              'symnorm' for symmetric normalized,
+              'diffusion' for density-corrected normalized.
+    N : int
+        Number of smallest non-trivial eigenvectors of the
+        graph Laplacian to be used for the construction of
+        local views.
+    no_gamma : bool
+               If True, the parameterization of local views
+               are not normalized.
+    Atilde_method : str
+                    Method to use for conmputing Atilde.
+                    Options are 'LDLE_1' for finite element method,
+                    'LLR' for local linear regression. Currently,
+                    only 'LDLE_1' works.
+    p : float
+        A hyperparameter used in computing Atilde. The value
+        must be in (0,1).
+    tau : int
+        A hyperparameter used in computing parameterizations
+        of local views. The value must be in (0,100).
+    delta : float
+        A hyperparameter used in computing parameterizations
+        of local views. The value must be in (0,1).
+    to_postprocess : bool
+        If True the local parameterizations are postprocessed
+        to fix anamolous parameterizations leading to high
+        distortion of the local views.
+    pp_n_thresh : int
+        Threshold to use multiple processors or a single processor
+        while postprocessing the local parameterizations. A small
+        value such as 32 leads to faster postprocessing.
+    """
+    return {'k_nn': k_nn, 'k_tune': k_tune, 'k': k,
+           'gl_type': gl_type, 'N': N, 'no_gamma': no_gamma,
+           'Atilde_method': Atilde_method, 'p': p, 'tau': tau,
+           'delta': delta, 'to_postprocess': to_postprocess, 'algo': algo,
+           'n_proc': n_proc, 'pp_n_thresh': pp_n_thresh}
 
-default_intermed_opts = {'eta_min': 5, 'eta_max': 100, 'len_S_thresh': 256,}
+def get_default_intermed_opts(eta_min=5, eta_max=100, len_S_thresh=256):
+    """Sets and returns a dictionary of default_intermed_opts.
+    
+    Parameters
+    ----------
+    eta_min : int
+              Minimum allowed size of the clusters underlying
+              the intermediate views. The values must be >= 1.
+    eta_max : int
+              Maximum allowed size of the clusters underlying
+              the intermediate views. The value must be > eta_min.
+    len_S_thresh : int
+                   Threshold on the number of points for which 
+                   the costs are to be updated, to invoke
+                   multiple processors.
+    """
+    return {'eta_min': eta_min, 'eta_max': eta_max, 'len_S_thresh': len_S_thresh}
+    
+def get_default_global_opts(main_algo='LDLE', to_tear=True, nu=3, max_iter=10,
+                            vis_before_init=False, compute_error=False,
+                            init_algo_name='sequential', init_algo_align_w_parent_only=True,
+                            refine_algo_name='retraction',
+                            refine_algo_max_internal_iter=100,
+                            refine_algo_alpha=0.3):
+    """Sets and returns a dictionary of default_global_opts.
 
-default_global_opts = {'to_tear': True, 'nu': 3, 'max_iter': 10,
-                       'vis_before_init': False,
-                       'compute_error': False,
-                       'main_algo': 'LDLE', # ['LDLE', 'LTSA']
-                       'init_algo': {
-                           'name': 'sequential', # ['sequential', 'spectral', 'sequential+spectral'], spectral ignores to_tear
-                           'align_w_parent_only': True
-                        },
-                       'refine_algo': {
-                           'name': 'retraction', # ['sequential', 'retraction', 'spectral']
-                           'max_internal_iter': 100, # 10 for sequential and 100 for retraction
-                           'alpha': 0.3, # step size for retraction
-                        }
-                      }
+    Parameters
+    ----------
+    main_algo : str
+                The algorithm to use for the alignment of intermediate
+                views. Options are 'LDLE' and 'LTSA'. If 'LTSA' is
+                chosen then none of the following hypermateters are used.
+    to_tear : bool
+              If True the tearing of the manifold is allowed.
+    nu : int
+         A hyperparameter used to compute neighboring intermediate
+         views in the embedding space.
+    max_iter : int
+               Number of iterations to refine the global embedding for.
+    vis_before_init : bool
+                      If True, plots the global embedding before
+                      alignment begins. This is same as just plotting
+                      all the intermediate views without alignment.
+    compute_error : bool
+                    If True the alignment error is computed at each 
+                    iteration of the refinement, otherwise only at
+                    the last iteration.
+    init_algo_name : str
+                     The algorithm used to compute initial global embedding
+                     by aligning the intermediate views.
+                     Options are 'sequential' for tree-based-procrustes alignment,
+                     'spectral' for spectral alignment (ignores to_tear),
+                     'sequential+spectral' for tree-based-procrustes alignment
+                     followed by spectral alignment.
+    init_algo_align_w_parent_only : bool
+                                    If True only the parents of the intermediate
+                                    views are used in the tree-based-procrustes
+                                    alignment.
+    refine_algo_name : str
+                       The algorithm used to refine the initial global embedding
+                       by refining the alignment between intermediate views.
+                       Options are 'sequential' for Generalized Procustes Analysis
+                       (GPA) based alignment, 'retraction' for Riemannian optimization
+                       based alignment, 'spectral' for spectral alignment.
+    refine_algo_max_internal_iter : int
+                                    The number of internal iterations used by
+                                    the refinement algorithm. This is ignored
+                                    by 'spectral' refinement.
+    refine_algo_alpha : int
+                        The step size used in the Riemannian gradient descent
+                        when the refinement algorithm is 'retraction'.
+    """
+    return {'to_tear': to_tear, 'nu': nu, 'max_iter': max_iter,
+               'vis_before_init': vis_before_init,
+               'compute_error': compute_error,
+               'main_algo': main_algo, # ['LDLE', 'LTSA']
+               'init_algo': {
+                   'name': init_algo_name, # ['sequential', 'spectral', 'sequential+spectral'], spectral ignores to_tear
+                   'align_w_parent_only': init_algo_align_w_parent_only
+                },
+               'refine_algo': {
+                   'name': refine_algo_name, # ['sequential', 'retraction', 'spectral']
+                   'max_internal_iter': refine_algo_max_internal_iter, # 10 for sequential and 100 for retraction
+                   'alpha': refine_algo_alpha, # step size for retraction
+                }
+              }
+def get_default_vis_opts(save_dir='', cmap_interior='summer', cmap_boundary='jet', c=None):
+    return {'save_dir': save_dir,
+             'cmap_interior': cmap_interior,
+             'cmap_boundary': cmap_boundary,
+             'c': c}
 
-default_vis_opts = {'save_dir': '',
-                     'cmap_interior': 'summer',
-                     'cmap_boundary': 'jet',
-                     'c': None}
 
 class LDLE:
-    """LDLE 
-        :param int d: Embedding dimension. [default=2]
-        :param dict local_opts: Options for local views construction. The key-value pairs
-                                provided override the ones in default_local_opts.
-        :param dict intermed_opts: Options for intermediate views construction. The key-value pairs
-                                provided override the ones in default_intermed_opts.
-        :param dict global_opts: Options for global views construction. The key-value pairs
-                                provided override the ones in default_global_opts.
-        :param dict global_opts: Options for global views construction. The key-value pairs
-                                provided override the ones in default_global_opts.
-        :param bool verbose: print logs if True. [default=False]
-        :param bool debug: saves intermediate objects/data for debugging. [default=False]
-        :rtype: object
+    """Low Dimensional Local Eigenmaps and its variations.
+    
+    Parameters
+    ----------
+    d : int
+       Embedding dimension.
+    local_opts : dict
+                Options for local views construction. The key-value pairs
+                provided override the ones in default_local_opts.
+    intermed_opts : dict
+                    Options for intermediate views construction. The key-value pairs
+                    provided override the ones in default_intermed_opts.
+    global_opts : dict
+                  Options for global views construction. The key-value pairs
+                  provided override the ones in default_global_opts.
+    vis_opts : dict
+               Options for visualization. The key-value pairs
+               provided override the ones in default_vis_opts.
+    n_proc : int
+             The number of processors to use. Defaults to approximately
+             3/4th of the available processors. 
+    verbose : bool
+              print logs if True.
+    debug : bool
+            saves intermediary objects/data for debugging.
     """
     def __init__(self,
-                 d = 2, # embedding dimension
-                 local_opts = {}, # see default_local_opts above
-                 intermed_opts = {}, # see default_intermed_opts above
-                 global_opts = {},# see default_global_opts above
-                 vis_opts = {}, # see default_vis_opts above
+                 d = 2,
+                 local_opts = {}, 
+                 intermed_opts = {},
+                 global_opts = {},
+                 vis_opts = {},
+                 n_proc = max(1,int(multiprocessing.cpu_count()*0.75)),
                  exit_at = None,
                  verbose = False,
                  debug = False):
-
+        default_local_opts = get_default_local_opts()
+        default_intermed_opts = get_default_intermed_opts()
+        default_global_opts = get_default_global_opts()
+        default_vis_opts = get_default_vis_opts()
+        
         self.d = d
+        local_opts['n_proc'] = n_proc
         for i in local_opts:
             default_local_opts[i] = local_opts[i]
         self.local_opts = default_local_opts
@@ -185,13 +353,13 @@ class LDLE:
                                       self.local_opts['k'])
         #############################################
         intermed_opts['algo'] = self.local_opts['algo']
-        intermed_opts['n_proc'] = self.local_opts['n_proc']
+        intermed_opts['n_proc'] = n_proc
         for i in intermed_opts:
             default_intermed_opts[i] = intermed_opts[i]
         self.intermed_opts = default_intermed_opts
         #############################################
         global_opts['k'] = self.local_opts['k']
-        global_opts['n_proc'] = self.local_opts['n_proc']
+        global_opts['n_proc'] = n_proc
         for i in global_opts:
             default_global_opts[i] = global_opts[i]
         self.global_opts = default_global_opts
@@ -236,7 +404,7 @@ class LDLE:
                                                           metric='euclidean')
             else:
                 self.log('Doubling manifold.')
-                neigh_dist, neigh_ind = double_manifold(X, ddX, self.local_opts['k_nn'])
+                neigh_dist, neigh_ind = double_manifold_k_nn(X, ddX, self.local_opts['k_nn'])
                 self.log('Done.', log_time=True)
         
         # Construct a sparse d_e matrix based on neigh_ind and neigh_dist
